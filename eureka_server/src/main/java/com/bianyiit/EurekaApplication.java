b6f342e221847ac9f5256c5320177a8c9dd1cbe9
zhaihui <348600452@qq.com>
2020-07-16 21:35:12 +0800

    创建eureka

---
+++ eureka_server/src/main/java/com/bianyiit/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.bianyiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //开启Eureka服务端
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
